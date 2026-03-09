1acdb9d3c35f6259c6516ab615b2c0ac58e9962b
xkrye <ver.sy098@gmail.com>
2026-03-09 14:55:53 +0800

    update the app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ dotenv.config();
 const connectDB = require('./config/db');
 const passport = require('./config/passport');
 
+dotenv.config();
+
 // Connect to MongoDB
 connectDB();
 
@@ -80,7 +82,7 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 
 
 // Mount routes
-app.use('/api/auth', authRoutes);
+app.use('/api/auth/google', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/residents', residentRoutes);
 app.use('/api/immunizations', immunizationRoutes);
